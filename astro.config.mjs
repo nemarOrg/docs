d79d31fb889b1baed22569ec69ac8857a0f1638e
Seyed Yahya Shirazi <shirazi@ieee.org>
2026-09-08 05:29:05 -0700

    feat: created date from first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,6 +24,7 @@ export default defineConfig({
 			components: {
 				Footer: './src/components/Footer.astro',
 				Header: './src/components/Header.astro',
+				LastUpdated: './src/components/LastUpdated.astro',
 			},
 			plugins: [
 				starlightLinksValidator({
